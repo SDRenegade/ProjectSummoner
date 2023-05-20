0289a3975ee4947d5dcc0b01e1d019b64c7c9166
Brody_Horton <HORTONB@uwplatt.edu>
2023-05-20 16:13:31 -0500

    Added jump mechanic

--- Project Summoner/Assets/Scripts/ThirdPersonMovement.cs
+++ Project Summoner/Assets/Scripts/ThirdPersonMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
@@ -12,8 +13,13 @@ public class ThirdPersonMovement : MonoBehaviour
     private float moveSpeed;
     [SerializeField]
     private float turnSmoothTime;
+    [SerializeField]
+    private float jumpVelocity;
+    [SerializeField]
+    private float gravity;
 
     private float turnSmoothVelocity;
+    private Vector3 moveVelocity;
 
     void Update()
     {
@@ -27,8 +33,29 @@ public class ThirdPersonMovement : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            characterController.Move(moveDir * moveSpeed * Time.deltaTime);
+            //Rotates Vector3.forward targetAngle degrees around the Y-axis
+            float temp = moveVelocity.y;
+            moveVelocity = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward * moveSpeed;
+            moveVelocity.y = temp;
+            //characterController.Move(moveVelocity * moveSpeed * Time.deltaTime);
         }
+        else {
+            moveVelocity.x = 0f;
+            moveVelocity.z = 0f;
+        }
+
+        if (characterController.isGrounded) {
+            moveVelocity.y = 0f;
+
+            if (Input.GetKey("c"))
+            {
+                Debug.Log("Jump has been pressed");
+                moveVelocity.y = jumpVelocity;
+            }
+        }
+
+        moveVelocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(moveVelocity * Time.deltaTime);
     }
 }
